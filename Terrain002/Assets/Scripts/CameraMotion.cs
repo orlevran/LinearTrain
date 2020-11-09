ac3f3c05dbb6f9511d908af818efe6edef976ad2
orlevran <orlevor@gmail.com>
2020-11-09 20:07:33 +0200

    LinearTrain

---
+++ Terrain002/Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    float mouseSensitivity = 500f;
+    public float mouseX, mouseY, xRotation = 0f;
+    public Transform Camera_Holder;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Camera_Holder.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Terrain002/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float x, z, speed = 50f;
+    Vector3 move;
+    public CharacterController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
