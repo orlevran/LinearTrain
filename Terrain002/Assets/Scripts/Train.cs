a5f8e0569ec1806a9d0c0f40c7acd20f86cda93d
Talisra <44340141+Talisra@users.noreply.github.com>
2020-11-19 19:18:45 +0200

    Revert "Train movement"
    This reverts commit 4676ceda1d809212ccbcc13dbd113ca99bcf5b4d.

--- Terrain002/Assets/Scripts/Train.cs
+++ Terrain002/Assets/Scripts/Train.cs
@@ -8,100 +8,42 @@ public class Train : MonoBehaviour
     public List<GameObject> Cars;
     public List<Transform> Stations;
 
-    private bool AtStation = false;
-    private float TrainLength = 0f;
-    private float TrainTopSpeed = 400.0f;
-    private float TrainMinSpeed = 10f;
-    private float AccelerationSpeed = 0.3f;
+    private bool isAccelerating = true;
 
-    private float DistanceSlowdown = 500f;
+    private float TrainLength = 0f;
+    private float TrainTopSpeed =1000.0f;
+    private float AccelerationSpeed = 0.05f;
+
     private float CurrentSpeed = 0;
     private int StationIndex = 0;
-
-    private int status = 0;
     // Start is called before the first frame update
     void Start()
     {
-        // calculates the train length to reposition the engine
         foreach(GameObject car in Cars)
         {
+
             TrainLength += car.GetComponent<Collider>().bounds.size.x; // remember to add a collider to new cars
         }
+        Debug.Log(TrainLength);
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch(status)
-        {
-            case 0:
-                Accelerate();
-                break;
-            case 1:
-                Drive();
-                break;
-            case 2:
-                SlowDown();
-                break;
-            case 3:
-                AriveAtStation();
-                break;
-            default:
-                break;
-        }
-    }
-
-    void Accelerate()
-    {
-        MoveTrain();
-        CurrentSpeed += AccelerationSpeed;
-        if (CurrentSpeed > TrainTopSpeed) // Make sure the train doesn't pass the top speed
-        {
-            CurrentSpeed = TrainTopSpeed;
-            status = 1;
-        }
-    }
-
-    void Drive()
-    {
-        MoveTrain();
-        if (Vector3.Distance(transform.position, Stations[StationIndex].position) < DistanceSlowdown)
-        {
-            status = 2;
-        }
-    }
-    void SlowDown()
-    {
-        MoveTrain();
-        CurrentSpeed -= AccelerationSpeed*1.5f;
-        if (CurrentSpeed < TrainMinSpeed)
-            CurrentSpeed = TrainMinSpeed;
-        if (Vector3.Distance(transform.position, Stations[StationIndex].position) < 0.001f)
-        {
-            status = 3;
-        }
-    }
-    void AriveAtStation()
-    {
-        if (!AtStation)
-        {
-            AtStation = true;
-            CurrentSpeed = 0;
-            StartCoroutine(WaitForPassengers());
-            IEnumerator WaitForPassengers()
-            {
-                yield return new WaitForSeconds(5);
-                NextStation();
-                status = 0;
-                AtStation = false;
-            }
-        }
-    }
-
-    void MoveTrain()
-    {
+        ManageSpeed();
         float Step = CurrentSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, Stations[StationIndex].position, Step);
+        if (Vector3.Distance(transform.position, Stations[StationIndex].position) < 0.001f)
+        {
+            isAccelerating = false;
+            AllAboard();
+            NextStation();
+        }
+    }
+
+    void AllAboard()
+    {
+
     }
 
     void NextStation()
@@ -112,6 +54,7 @@ public class Train : MonoBehaviour
         }
         else
             StationIndex = 0;
+        isAccelerating = true;
         Vector3 engineOffset;
         if (StationIndex == 0)
             engineOffset = new Vector3(-TrainLength, 0, +4.456f);
@@ -121,6 +64,23 @@ public class Train : MonoBehaviour
         Engine.transform.position += engineOffset;
         //Debug.Log(Engine.transform.position);
         Engine.transform.LookAt(Stations[StationIndex]);
+    }
+
+    void ManageSpeed()
+    {
+        if (isAccelerating)
+        {
+            CurrentSpeed += AccelerationSpeed; //Accelerate the train
+        }
+        else if (CurrentSpeed > 0)
+        {
+            CurrentSpeed -= AccelerationSpeed*10; //Decelerate faster
+        }
+
+        if (CurrentSpeed > TrainTopSpeed) // Make sure the train doesn't pass the top speed
+        {
+            CurrentSpeed = TrainTopSpeed;
+        }
     }
 
 }
