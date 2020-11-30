cf7a4022888cdf83b3b0b7f1200d0712c5807188
orlevran <orlevor@gmail.com>
2020-11-30 22:30:35 +0200

    NavMesh

---
+++ Terrain002/Assets/Scripts/CapsuleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CapsuleManager : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject train;
+    public GameObject trainStopPos;
+
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(trainStopPos.transform.position, train.transform.position) < 1)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+    }
+}
